012a6cc68690a19dffaad6e24288810337212646
Martin Gardner II <martingardnerii@gmail.com>
2024-01-05 09:18:08 -0600

    timers base example

--- baseExamples/range-channels.go
+++ baseExamples/range-channels.go
@@ -7,7 +7,7 @@ func main() {
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
-	close(queue)
+	close(queue) // close channel, can still print out below what is in it
 
 	for elem := range queue {
 		fmt.Println(elem)
